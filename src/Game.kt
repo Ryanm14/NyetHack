107d3e2f1660cb2566b5d881c80f8cadf972a1dc
Ryan Miles <ryanm1114@gmail.com>
2019-05-03 23:13:09 -0400

    base

--- src/Game.kt
+++ src/Game.kt
@@ -1,10 +1,5 @@
 fun main(){
-    val name = "Madrigal"
-    var healthPoints = 100
-
-    if (healthPoints == 100) {
-        println(name + " is in excellent condition!")
-    } else {
-        println(name + " is in awful condition!")
-    }
+    var isBlessed = true
+    // Player status
+    println("(Blessed: ${if (isBlessed) "YES" else "NO"})")
 }
